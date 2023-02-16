0568357df4ee289e0bf4ee76cb05af425876026b
marjan mastaliparsa <marjan.mastaliparsa@riddleandcode.com>
2023-02-16 13:05:34 +0100

    Fix some bugs

--- app/database/machine.py
+++ app/database/machine.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app.database.schema import DaoMachine
@@ -7,6 +8,9 @@ from app.models.machine import MachineCreate
 
 
 def save_machine(session: Session, machine: MachineCreate) -> DaoMachine:
+    machine = fetch_machine_by_machine_id(session, machine.machine_id)
+    if machine:
+        raise HTTPException(status_code=400, detail='Machine_id (public-key) exist.')
     machine = DaoMachine(machine_id=machine.machine_id, machine_type=machine.machine_type)
     session.add(machine)
     session.commit()
@@ -19,3 +23,7 @@ def fetch_machines(session: Session) -> List[DaoMachine]:
 
 def fetch_machine_by_id(session: Session, id: int) -> DaoMachine:
     return session.query(DaoMachine).filter_by(id=id).first()
+
+
+def fetch_machine_by_machine_id(session: Session, machine_id: str) -> DaoMachine:
+    return session.query(DaoMachine).filter_by(machine_id=machine_id).first()
--- app/database/time_series_data.py
+++ app/database/time_series_data.py
@@ -1,13 +1,18 @@
 import datetime
 from typing import List
 
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
+from app.database.machine import fetch_machine_by_id
 from app.database.schema import DaoTimeSeriesData
 from app.models.time_series_data import TimeSeriesDataCreate
 
 
 def save_time_series_data(session: Session, data: TimeSeriesDataCreate) -> DaoTimeSeriesData:
+    machine = fetch_machine_by_id(session, data.machine_id)
+    if not machine:
+        raise HTTPException(status_code=404, detail='Machine not found.')
     data = DaoTimeSeriesData(timestamp=data.timestamp, absolute_energy=data.absolute_energy, unit=data.unit,
                              machine_id=data.machine_id, cid=data.cid if data.cid else None)
     session.add(data)
@@ -23,5 +28,10 @@ def fetch_time_series_data_by_machine_id(session: Session, machine_id: int) -> L
     return session.query(DaoTimeSeriesData).filter_by(machine_id=machine_id).all()
 
 
-def fetch_time_series_data_by_machine_id_in_range(session: Session, machine_id: int, start: datetime, end: datetime) -> List[DaoTimeSeriesData]:
-    return session.query(DaoTimeSeriesData).filter_by(machine_id=machine_id).filter(DaoTimeSeriesData.timestamp.between(start, end)).all()
+def fetch_time_series_data_by_machine_id_in_range(session: Session, machine_id: int, start: datetime, end: datetime) -> \
+List[DaoTimeSeriesData]:
+    machine = fetch_machine_by_id(session, machine_id)
+    if not machine:
+        raise HTTPException(status_code=404, detail='Machine not found.')
+    return session.query(DaoTimeSeriesData).filter_by(machine_id=machine_id).filter(
+        DaoTimeSeriesData.timestamp.between(start, end)).all()
