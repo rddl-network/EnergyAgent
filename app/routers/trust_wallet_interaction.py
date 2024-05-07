8e62ce06830c75e57681fbc920b8262e650986bd
Stefan Weber <stefan.weber93@googlemail.com>
2024-05-07 09:59:17 +0200

    fix black
    Signed-off-by: Stefan Weber <stefan.weber93@googlemail.com>

--- app/routers/trust_wallet_interaction.py
+++ app/routers/trust_wallet_interaction.py
@@ -12,16 +12,17 @@ router = APIRouter(
 )
 
 # system pick and optimistic architecture selection
-if platform.system() == 'Linux':
-    if platform.processor() == 'x86_64':
+if platform.system() == "Linux":
+    if platform.processor() == "x86_64":
         trust_wallet = TrustWalletInteraction("lib/linux/x86_64/libocc.so", "/dev/ttyACM0")
     else:
         trust_wallet = TrustWalletInteraction("lib/linux/armv7/libocc.so", "/dev/ttyACM0")
-elif platform.system() == 'Darwin':
+elif platform.system() == "Darwin":
     trust_wallet = TrustWalletInteraction("lib/macos/aarch/libpyocc.dylib", "/dev/ttyACM0")
 else:
     sys.exit("unsupported OS, cannot load TA Wallet connector")
-    
+
+
 @router.get("/valise-get")
 def valise_get():
     return trust_wallet.valise_get()
