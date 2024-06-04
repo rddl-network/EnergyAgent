011c4a1f8600f04722fc15ede184eba0b148a287
Jürgen Eckel <juergen@riddleandcode.com>
2024-06-04 17:10:56 +0200

    blackified
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- app/main.py
+++ app/main.py
@@ -25,18 +25,20 @@ async def startup_event():
     ag.setup()
     await ag.run()
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Starting up...")
-    asyncio.create_task( startup_event() )
+    asyncio.create_task(startup_event())
     yield  # This is where your application starts handling requests
     print("Shutting down...")
+
 
 app = FastAPI(
     title="Rddl Energy Agent",
     description="Set of tools that help to interact with the RDDL Network and offer services that are domain agnostic",
     version="1.0.0",
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 app.add_middleware(
