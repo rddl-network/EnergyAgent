8b835dafddd9bac8423a8a00b6c685909db40916
Stefan Weber <8808492+Cybnon@users.noreply.github.com>
2024-06-17 15:17:19 +0200

    Cybnon/add mainnet testnet switch (#65)
    * Use prebuild docker container
    * Display MachineId
    * Implement network switch
    
    
    Signed-off-by: Stefan Weber <stefan.weber93@googlemail.com>

--- app/routers/trust_wallet_interaction.py
+++ app/routers/trust_wallet_interaction.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
 from app.RddlInteraction.TrustWallet.osc_message_sender import is_not_connected
-from app.RddlInteraction.planetmint_interaction import planetmint_slot
+from app.RddlInteraction.planetmint_interaction import planetmint_slot, pre_attest_slot
 from app.dependencies import trust_wallet_instance, config
 from app.helpers.models import PlanetMintKeys
 
@@ -41,3 +41,11 @@ def get_planetmint_keys() -> PlanetMintKeys:
         raise HTTPException(status_code=400, detail="wallet not connected")
     planetmint_keys = trust_wallet_instance.get_planetmint_keys()
     return planetmint_keys
+
+
+@router.get("/get-machine-id")
+def get_machine_id() -> str:
+    if is_not_connected(config.trust_wallet_port):
+        raise HTTPException(status_code=400, detail="wallet not connected")
+    machine_id = trust_wallet_instance.get_public_key_from_se050(pre_attest_slot)
+    return machine_id
