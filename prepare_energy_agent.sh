e56bd535034cab8ae5baede610c578c00a70f17d
Stefan Weber <stefan.weber93@googlemail.com>
2024-06-20 13:38:03 +0200

    Fix prepare_energy_agent.sh and fix provision.py
    Signed-off-by: Stefan Weber <stefan.weber93@googlemail.com>

--- prepare_energy_agent.sh
+++ prepare_energy_agent.sh
@@ -8,18 +8,26 @@ sudo apt-get upgrade -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
+# Add current user to the Docker group
+sudo usermod -aG docker "${USER}"
+
+# Use newgrp docker to run the subsequent commands in a new shell with the updated group
+newgrp docker << EOF
+
 # Create a directory for the docker-compose file
 mkdir -p ~/energy-agent
 cd ~/energy-agent || exit
 
 # Download the docker-compose file from GitHub
 # Replace the URL with the actual URL of your docker-compose file
-curl -L https://github.com/rddl-network/EnergyAgent/blob/main/docker-compose.yaml -o docker-compose.yml
+wget https://github.com/rddl-network/EnergyAgent/raw/main/docker-compose.yaml
 
 # Start the Docker Compose
-sudo docker compose up -d
+docker compose up -d
 
 # Cleanup
-rm get-docker.sh
+rm ../get-docker.sh
 
 echo "Docker and Docker Compose setup complete. The services are now running."
+
+EOF
