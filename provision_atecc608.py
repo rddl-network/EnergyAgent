9c9ad22611547fdab18b977221914ed2508a35e6
cybnon <stefan.weber93@googlemail.com>
2024-09-13 09:54:28 +0200

    uncommend zone locking
    Signed-off-by: cybnon <stefan.weber93@googlemail.com>

--- provision_atecc608.py
+++ provision_atecc608.py
@@ -6,8 +6,10 @@ from app.RddlInteraction.TrustWallet.TrustWalletConnectorATECC608 import TrustWa
 
 trust_wallet = TrustWalletConnectorATECC608()
 
+
 def print_hex_buffer(input):
     print(" ".join(f"{x:02X}" for x in input))
+
 
 def write_atecc_config():
     status = trust_wallet.atecc608_lib.atecc_handler_init(0xC0, 1)
@@ -21,10 +23,11 @@ def write_atecc_config():
         print(f"atecc_handler_write_configuration Fail! {status}")
         return
 
-    status = trust_wallet.atecc608_lib.atecc_handler_lock_zone(0)
-    if status:
-        print(f"atecc_handler_lock_zone Fail! {status}")
-        return
+    # status = trust_wallet.atecc608_lib.atecc_handler_lock_zone(0)
+    # if status:
+    #     print(f"atecc_handler_lock_zone Fail! {status}")
+    #     return
+
 
 def provision_atecc():
     status = trust_wallet.atecc608_lib.atecc_handler_init(0xC0, 1)
@@ -66,9 +69,68 @@ def provision_atecc():
     return 0
 
 
+def inject_priv_key():
+    priv_key = (c_uint8 * 36)(
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x39,
+        0xAC,
+        0x9B,
+        0xF9,
+        0x17,
+        0x1D,
+        0xE8,
+        0x6F,
+        0xFA,
+        0x77,
+        0xE0,
+        0xB9,
+        0x05,
+        0x0B,
+        0xF6,
+        0xE0,
+        0x6A,
+        0x2C,
+        0x1B,
+        0xC1,
+        0x76,
+        0x79,
+        0x36,
+        0xE6,
+        0xC7,
+        0x45,
+        0x79,
+        0xE4,
+        0x26,
+        0xA4,
+        0x47,
+        0x5F,
+    )
+
+    slotID = 1
+    pub_key = (c_uint8 * 64)()
+
+    status = trust_wallet.atecc608_lib.atecc_handler_init(0xC0, 1)
+    if status:
+        print(f"atecc_handler_init Fail! {status}")
+        return 0
+
+    status = trust_wallet.atecc608_lib.atecc_handler_inject_priv_key(slotID, priv_key)
+    if status:
+        print(f"atecc_handler_inject_priv_key Fail! {status}")
+        return 0
+
+    status = trust_wallet.atecc608_lib.atecc_handler_get_public_key(slotID, pub_key)
+    if status:
+        print(f"atecc_handler_get_public_key Fail! {status}")
+        return 0
+    print("Pub key:")
+    print_hex_buffer(pub_key)
+
+
 if __name__ == "__main__":
     write_atecc_config()
     # provision_atecc()
-
-
-    
+    # inject_priv_key()
