2bde7f88bcb07672c37848c48043e58916eeda5e
cybnon <stefan.weber93@googlemail.com>
2024-11-06 13:13:19 +0100

    Restart smart meter reader when device gets restartet
    Signed-off-by: cybnon <stefan.weber93@googlemail.com>

--- app/main.py
+++ app/main.py
@@ -41,10 +41,8 @@ async def lifespan(app: FastAPI):
     energy_manager = get_energy_agent_manager()
     await energy_manager.check_and_restart()
 
-    was_smart_meter_running = load_config(config.metadata_config_path).get("status", "stopped") == "running"
-    if was_smart_meter_running:
-        sm_manager = get_smart_meter_manager()
-        await sm_manager.check_and_restart()
+    sm_manager = get_smart_meter_manager()
+    await sm_manager.check_and_restart()
 
     yield  # This is where your application starts handling requests
     print("Shutting down...")
