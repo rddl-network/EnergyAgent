e1a2259ecbb5ad4d608f786e2d6fe7ae8b709074
Jürgen Eckel <juergen@riddleandcode.com>
2025-01-31 10:57:01 +0100

    fixed kwh vs wh issue
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- app/energy_agent/cerbogx.py
+++ app/energy_agent/cerbogx.py
@@ -7,6 +7,5 @@ def process_production_readout(topic: str, data: str):
     if config.production_readout_mode != PRODUCTION_READOUT_MODE_CERBOGX:
         return
     if re.match(config.production_readout_pattern, topic):
-        wh_production = float(data)
-        kwh_production = Measurements.convert_to_kwh(wh_production)
+        kwh_production = float(data)
         measurement_instance.set_abs_production_value(kwh_production)
