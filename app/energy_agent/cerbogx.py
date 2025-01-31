c3f001e7d5fbd2e6115ca58619ce0a50e7ed3c38
Jürgen Eckel <juergen@riddleandcode.com>
2025-01-31 13:35:33 +0100

    process json object instead of direct floats
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- app/energy_agent/cerbogx.py
+++ app/energy_agent/cerbogx.py
@@ -1,11 +1,14 @@
+import json
 import re
 from app.dependencies import PRODUCTION_READOUT_MODE_CERBOGX, config, measurement_instance
-from app.model.measurements import Measurements
+from app.helpers.logs import logger
 
 
 def process_production_readout(topic: str, data: str):
     if config.production_readout_mode != PRODUCTION_READOUT_MODE_CERBOGX:
         return
-    if re.match(config.production_readout_pattern, topic):
-        kwh_production = float(data)
+    if re.match(config.production_readout_pattern, topic):        
+        value_object = json.loads(data)
+        kwh_production = float(value_object["value"])
+        logger.info(f"Notarization: {kwh_production}, {topic}")
         measurement_instance.set_abs_production_value(kwh_production, topic)
