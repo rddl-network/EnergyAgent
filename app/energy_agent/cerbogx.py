4f4d9478ffa7bc42d7f7254030e7bafaf11c9268
Jürgen Eckel <eckelj@users.noreply.github.com>
2025-01-31 11:09:15 +0100

    added pattern definition via config (#124)
    * added pattern definition via config
    * changed the default query value
    
    ---------
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- app/energy_agent/cerbogx.py
+++ app/energy_agent/cerbogx.py
@@ -6,7 +6,6 @@ from app.model.measurements import Measurements
 def process_production_readout(topic: str, data: str):
     if config.production_readout_mode != PRODUCTION_READOUT_MODE_CERBOGX:
         return
-    if re.match(r"^N/[^/]+/system/0/Dc/Pv/Power$", topic):
-        wh_production = float(data)
-        kwh_production = Measurements.convert_to_kwh(wh_production)
+    if re.match(config.production_readout_pattern, topic):
+        kwh_production = float(data)
         measurement_instance.set_abs_production_value(kwh_production)
